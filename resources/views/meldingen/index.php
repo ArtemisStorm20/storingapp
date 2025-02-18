c8eb3c58acea9fddf4dd7b01b74da3d880dc121c
ArtemisStorm20 <D309906@edu.curio.nl>
2025-02-18 12:16:42 +0100

    Update index.php

--- resources/views/meldingen/index.php
+++ resources/views/meldingen/index.php
@@ -32,14 +32,24 @@
             $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
         ?>
 
-        <?php foreach ($meldingen as $melding): ?>
-            <h1><?php echo $melding['attractie'] ?></h1>
-            <h1><?php echo $melding['type'] ?></h1>
-            <h1><?php echo $melding['capaciteit'] ?></h1>
-            <h1><?php echo $melding['prioriteit'] ?></h1>
-            <h1><?php echo $melding['melder'] ?></h1>
-            <h1><?php echo $melding['overige_info'] ?></h1>
+        <table>
+            <tr>
+               <th>Attractie</th>
+               <th>Type</th>
+               <th>Melder</th>
+               <th>Overige info</th> 
+            </tr>
+            <?php foreach ($meldingen as $melding): ?>
+            <tr>
+                <th><h1><?php echo $melding['attractie'] ?></h1></th>
+                <th><h1><?php echo $melding['type'] ?></h1></th>
+                <th><h1><?php echo $melding['capaciteit'] ?></h1></th>
+                <th><h1><?php echo $melding['prioriteit'] ?></h1></th>
+                <th><h1><?php echo $melding['melder'] ?></h1></th>
+                <th><h1><?php echo $melding['overige_info'] ?></h1></th>
+            </tr>
         <?php endforeach; ?>
+        </table>
     </div>
 
 </body>
